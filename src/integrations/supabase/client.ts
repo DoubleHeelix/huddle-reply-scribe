6701c8de7b20160e6e2dacef017609c0456912f4
Shaheel kumar <heelixkumar98@gmail.com>
2025-06-16 13:01:53 +1200

    Deploy to Railway

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://whcpcqcesigjczcxxiwk.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoY3BjcWNlc2lnamN6Y3h4aXdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5NzMwMDQsImV4cCI6MjA2NTU0OTAwNH0.cZmez8B1_-QRk7EtSlm-KPw8vDwpxo8gRtnZ_Vd60Z0";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
